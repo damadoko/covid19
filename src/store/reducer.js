48a6414f3ac9042aa7475bd917dd720f611313d4
Le Minh Triet <leminhtriet@Les-MacBook-Pro.local>
2020-04-15 15:16:09 +0700

    setup redux: create 'reducer' and 'initialState'

---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+ currentFilter = null,
+ top3Country= [],
+ hotNews: []
+}
+
+const reducer = (state = initialState, action) => {
+ return state
+}
+
+export default reducer
